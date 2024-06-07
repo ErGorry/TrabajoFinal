c0212edbaf960c04ad482938c65406a149b27db1
Alvaro Babarro <108337462+ErGorry@users.noreply.github.com>
2024-06-07 11:30:47 +0200

    Update create.sql de reservas

--- MicroReservas/src/main/resources/static/create.sql
+++ MicroReservas/src/main/resources/static/create.sql
@@ -1,4 +1,4 @@
 DROP SCHEMA IF EXISTS reservas;
 CREATE SCHEMA reservas;
 DROP TABLE IF EXISTS reservas.reserva;
-CREATE TABLE reservas.reserva (id_reserva INT NOT NULL AUTO_INCREMENT, nombre_cliente VARCHAR(255), dni VARCHAR(255), id_hotel INT, id_vuelo INT,num_personas INT, PRIMARY KEY(id_reserva));
+CREATE TABLE reservas.reserva (id_reserva INT NOT NULL AUTO_INCREMENT, nombre_cliente VARCHAR(255), dni VARCHAR(255), id_hotel INT, id_vuelo INT,numero_personas INT, PRIMARY KEY(id_reserva));
